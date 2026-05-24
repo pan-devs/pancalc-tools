40d344bc9113ce3ab147e6f9f0768e7c413b4ff0
ag4pornis <adrin_bing2@outlook.es>
2026-05-24 15:26:34 +0200

    chore: bump to v0.1.3

--- pcalc/cli.py
+++ pcalc/cli.py
@@ -36,7 +36,7 @@ class AsciiBarColumn(ProgressColumn):
             result.append("░" * (self.bar_width - filled), style=theme.PRIMARY)
         return result
 
-VERSION = "0.1.2"
+VERSION = "0.1.3"
 
 
 # ---------------------------------------------------------------------------
