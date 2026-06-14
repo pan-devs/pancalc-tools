6b51901a51783b532e79723202475b83e750a26b
ag4pornis <adrin_bing2@outlook.es>
2026-06-14 14:08:15 +0200

    v0.2.4

--- pcalc/cli.py
+++ pcalc/cli.py
@@ -36,7 +36,7 @@ class AsciiBarColumn(ProgressColumn):
             result.append("░" * (self.bar_width - filled), style=theme.PRIMARY)
         return result
 
-VERSION = "0.2.3"
+VERSION = "0.2.4"
 
 
 # ---------------------------------------------------------------------------
